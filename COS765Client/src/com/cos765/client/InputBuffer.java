aa2b3afbd2051bb7943025f876ceed51d60e62e1
gustavogbc@gmail.com <gustavogbc@gmail.com@5bbf33ad-1208-a7a3-d8ee-a7730921b824>
2014-06-21 01:42:47 +0000

    recomeçando a pensar o Buffer

--- COS765Client/src/com/cos765/client/InputBuffer.java
+++ COS765Client/src/com/cos765/client/InputBuffer.java
@@ -3,45 +3,10 @@ package com.cos765.client;
 import java.util.LinkedList;
 
 import com.cos765.common.Segment;
-
-// O papel do InputBuffer é receber todos os dados NO cliente, 
-//gerar o atraso necessário, reordenar os pacotes 
-//e só então enviar os mesmos para a aplicação 
+ 
 public class InputBuffer {
 
-	private static InputBuffer instance = null; //singleton para o buffer
 	private static LinkedList<Segment> buffer = new LinkedList<Segment>();
 	private static int MAX_SIZE = 10; // tamanho default, apenas um chute.
-	private static BufferReader consumer;
-
-	private InputBuffer() {
-
-	}
-
-	public static InputBuffer getInstance() {
-		if (instance == null)
-			instance = new InputBuffer();
-		return instance;
-	}
-
-	public static int getSize() {
-		return buffer.size();
-	}
-
-	public static void registerConsumer(BufferReader consumer) {
-		InputBuffer.consumer = consumer;
-	}
-
-	public static void add(Segment segment) {
-		buffer.add(segment);
-
-		// "vocˆe deve esperar o buffer encher para come¸car a ler os pacotes."	
-		if (buffer.size() == MAX_SIZE)
-			consumer.consumeBuffer();
-	}
-
-	public static Segment consume() { 
-		return buffer.removeFirst();
-	}
 
 }
