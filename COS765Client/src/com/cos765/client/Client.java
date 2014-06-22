08c0140b50e2087cf6dd8974f91e39df8fa77f75
gustavogbc@gmail.com <gustavogbc@gmail.com@5bbf33ad-1208-a7a3-d8ee-a7730921b824>
2014-06-22 13:30:46 +0000

    mudando nome das classes Client -> _Client ProducerConsumerSolution -> Client
    agora posso apagar _Client

--- COS765Client/src/com/cos765/client/Client.java
+++ COS765Client/src/com/cos765/client/Client.java
@@ -1,19 +1,35 @@
 package com.cos765.client;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Vector;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import com.cos765.common.Common;
 import com.cos765.common.Segment;
 
 public class Client {
-			
-	public static void main(String[] args) throws Exception {
+
+	public static void main(String args[]) throws SocketException, UnknownHostException {
+		
+		Vector buffer = new Vector();
+		LinkedBlockingQueue<Integer> delayList = new LinkedBlockingQueue<Integer>();
+		
+		final int MAX_BUFFER_SIZE = 4;
+		Thread producer = new Thread(new Producer(buffer, MAX_BUFFER_SIZE, delayList),
+				"Produtor");
+		Thread consumer = new Thread(new BufferConsumer(buffer, MAX_BUFFER_SIZE),
+				"Consumidor");
+		producer.start();
+		consumer.start();
+		
+		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
@@ -21,11 +37,9 @@ public class Client {
 		byte[] receiveData = new byte[Segment.PAYLOAD_SIZE + Segment.HEADER_SIZE]; // bytes do arquivo recebido 
 		byte[] payload = new byte[Segment.PAYLOAD_SIZE]; 
 		long receiveTime = 0;
-		byte sendOrder = 0;
-
-//		InputBuffer inBuffer = new InputBuffer();
-//		BufferConsumer consumer = new BufferConsumer(inBuffer);
-					
+		byte sendOrder = 0;	
+	
+			
 		try {
 			// Cliente informa o nome do arquivo desejado
 			String fileName = inFromUser.readLine(); 
@@ -43,19 +57,80 @@ public class Client {
 				payload = Arrays.copyOfRange(receiveData, 1, receiveData.length - 1);
 
 				Segment segment = new Segment(sendOrder, payload, receiveTime);
-//				inBuffer.add(segment);
-				//TODO: levar para camada de atraso
-				//TODO: levar para camada de buffer
-				//TODO: levar para camada de aplicação (esta de novo?) 
+				
+				delayList.put((int) sendOrder);	
+				System.out.println("Chegou via UDP: " + delayList.toString());
 
 				// TODO: registrar log do que foi recebido
-				String modifiedSentence = new String(receivePacket.getData());
-				System.out.println("FROM SERVER: " + modifiedSentence);
+//				String modifiedSentence = new String(receivePacket.getData());
+//				System.out.println("FROM SERVER: " + modifiedSentence);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			clientSocket.close();
+		}		
+	}
+}
+
+//Vai implementar a camada de simulação de atrasos e perda de dados
+//1 - Vai receber cada pacote da camada UDP
+//2 - 
+class Producer implements Runnable { 
+
+	private Vector buffer;
+	private final int SIZE;
+	private LinkedBlockingQueue<Integer> list;
+
+	public Producer(Vector buffer, int size, LinkedBlockingQueue<Integer> list) {
+		this.buffer = buffer;
+		this.SIZE = size;
+		this.list = list;
+	}
+
+	@Override
+	public void run() {
+		while(true)
+		{
+			
+			
+			try {
+				produce((int)list.take());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+//		for (int i = 0; i < 7; i++) {
+//			try {
+//				produce(i);
+//				Thread.sleep(20);
+//			} catch (InterruptedException ex) {
+//			}
+//		}
+	}
+
+	private void produce(int i) throws InterruptedException {
+
+		while (buffer.size() == SIZE) {
+			synchronized (buffer) {
+				System.out.println("Buffer cheio. "
+						+ Thread.currentThread().getName()
+						+ " esperando, size: " + buffer.size());
+				buffer.wait();
+			}
+		}
+
+		// producing element and notify consumers
+		synchronized (buffer) {
+			buffer.add(i);
+			if (buffer.size() == SIZE)
+				Common.bufferFull = true;
+			System.out.println("P: " + buffer.toString());
+			buffer.notifyAll(); // só permite consumir quando esteve cheio em
+								// algum momento
 		}
 	}
 }
